a07d3d509862ab642ac718af134c93bfdb64b908
Dmitrii Gritsai <gritsdmi@fel.cvut.cz>
2020-04-29 11:33:32 +0200

    hotel test

--- TripGuide/src/test/java/cz/cvut/fel/rsp/tripguide/service/HotelServiceTest.java
+++ TripGuide/src/test/java/cz/cvut/fel/rsp/tripguide/service/HotelServiceTest.java
@@ -1,11 +1,19 @@
 package cz.cvut.fel.rsp.tripguide.service;
 
+import cz.cvut.fel.rsp.tripguide.Generator;
+import cz.cvut.fel.rsp.tripguide.exception.NotFoundException;
+import cz.cvut.fel.rsp.tripguide.model.Hotel;
+import cz.cvut.fel.rsp.tripguide.model.Tour;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,5 +24,47 @@ public class HotelServiceTest {
     @Autowired
     public HotelService hotelService;
 
+    @Autowired
+    public UserService userService;
+
+    @Autowired
+    public TourService tourService;
+
+    @Test
+    public void addHotelTest() {
+        Hotel hotel = Generator.generateHotel();
+        assertNotNull(hotelService.save(hotel));
+
+        Tour tour = Generator.generateTour();
+        hotel = Generator.generateHotel();
+        assertNotNull(hotelService.addHotel(hotel, tour));
+
+        tour = tourService.save(Generator.generateTour());
+        hotel = Generator.generateHotel();
+        assertNotNull(hotelService.addHotel(hotel, tour.getId()));
+
+        tour = tourService.save(Generator.generateTour());
+        hotel = hotelService.save(Generator.generateHotel());
+        assertNotNull(hotelService.addHotel(hotel.getId(), tour.getId()));
+
+    }
+
+    @Test
+    public void getHotelTest() {
+        Hotel hotel = Generator.generateHotel();
+        hotelService.save(hotel);
+
+        assertEquals(hotel, hotelService.findHotel(hotel.getId()));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void removeHotelTest() {
+        Hotel hotel = Generator.generateHotel();
+        hotelService.save(hotel);
+
+        hotelService.remove(hotel);
+
+        hotelService.findHotel(hotel.getId());
+    }
 
 }
